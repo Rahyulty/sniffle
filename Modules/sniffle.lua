bc5c7aee5f23be8512f09557db5cdfbc6c60ede3
Rahyulty <ramlalajadhi@gmail.com>
2022-04-28 14:40:59 +0000

    Incomplete Push - Configuring some files

--- Modules/sniffle.lua
+++ Modules/sniffle.lua
@@ -1,6 +1,7 @@
-local Sniffle = { _version = "0.0.1"}
+local Sniffle = { _version = "0.0.2"}
 JSON = require "Modules.JSON"
 local lfs = require "lfs"
+
 
 local IsDir = function(name)
    if type(name)~="string" then return false end
@@ -8,6 +9,16 @@ local IsDir = function(name)
    local is = lfs.chdir(name) and true or false
    lfs.chdir(cd)
    return is   
+end
+
+function isFile(name)
+   if type(name)~="string" then return false end
+   if not IsDir(name) then
+       return os.rename(name,name) and true or false
+       -- note that the short evaluation is to
+       -- return false instead of a possible nil
+   end
+   return false
 end
 
 local Find = function (Table, Value)
@@ -26,7 +37,14 @@ end
 
 
 
-local Say = print
+local SniffleSay = print
+local Annotations = false
+
+local function SniffleHelper(value)
+   if Annotations == true then 
+   SniffleSay(value)
+   else return end 
+end
 
 Trees = {}
 
@@ -35,47 +53,105 @@ local GetPath = function(str,sep)
    return str:match("(.*"..sep..")")
 end
 
+function Sniffle.EnableSniffleHelper(val)
+   if val then
+      assert(type(val) == "boolean", "Arg #1 needs to be boolean instead got "..type(val))
+      Annotations = val
+      if val == true then 
+         print("Enabled SniffleHelper")
+      end
+   end 
+end
+
 function Sniffle.CreateDataTree(TreeName, Path)
    if IsDir(TreeName) == false then 
       if  Path == nil then 
-         Say("Dir not found creating Directory")
-         print(lfs.currentdir())
+         warn("Path not found creating Directory in workspace")
          MakeDirectory(TreeName)
+         File = io.open(lfs.currentdir().."/".."TreeManager", "w")
+         table.insert( Trees, #Trees + 1 ,TreeName)
+         local StringTable = JSON.encode(Trees)
+         File:write(StringTable)
+         File:close()
       elseif Path ~= nil then 
          assert(type(Path) == "string", " Your path needs to be a string")
          MakeDirectory(Path.."/"..TreeName)
+         File = io.open(lfs.currentdir().."/".."TreeManager", "w")
          table.insert( Trees, #Trees + 1 ,TreeName)
-
+         local StringTable = JSON.encode(Trees)
+         File:write(StringTable)
+         File:close()
        end
 
+
       else
-         table.insert( Trees, #Trees + 1 ,TreeName)
-         print("[~001~]The file "..TreeName.." has already been created, CreateTree has not been run ")
+         local File = io.open(lfs.currentdir().."/".."TreeManager",'r')
+         local String = File:read('*all')
+         local TableRaw = JSON.decode(String)
+         Trees = TableRaw
+         SniffleSay("[Sniffle Helper] The file "..TreeName.." has already been created already exist, no changes have been made")
    end
 end
 
 
 function Sniffle:CreateDataBase(DataBaseName, Under, PathofUnder)
-   if Find(Trees, Under) then 
-      if IsDir(Under) == true then 
-         if PathofUnder == nil then 
-            -- We Assume in workspace
-            File = io.open(lfs.currentdir().."/"..Under.."/"..DataBaseName, "w")
-            File:write(JSON.encode({}))
-            File:close()           
+   print(isFile(lfs.currentdir().."/"..Under.."/"..DataBaseName))
+  if PathofUnder == nil then
+   if isFile(lfs.currentdir().."/"..Under.."/"..DataBaseName) == false then 
+      if Find(Trees, Under) then 
+         if IsDir(Under) == true then 
+               File = io.open(lfs.currentdir().."/"..Under.."/"..DataBaseName, "w")
+               File:write(JSON.encode({}))
+               File:close()      
+            end
          else
-            File = io.open(PathofUnder..DataBaseName, "w")
-            File:write(JSON.encode({}))
-            File:close()      
+            self:CreateDataTree(Under)
+            SniffleSay("Directory does not exist one has been created for you in your "..lfs.currentdir())
+  end
+ end
+   else
+      if isFile(PathofUnder..DataBaseName) == false then 
+         if Find(Trees, Under) then 
+            if IsDir(Under) == true then 
+                  File = io.open(PathofUnder..DataBaseName, "w")
+                  File:write(JSON.encode({}))
+                  File:close()      
+               end
+            else
+               self:CreateDataTree(Under)
+               SniffleSay("Directory does not exist one has been created for you in your "..lfs.currentdir())
+            end
          end
-      else
-         self:CreateDataTree(Under)
-         warn("Directory does not exist one has been created for you in your "..lfs.currentdir())
       end
-   end
 end
 
 
 
+function Sniffle:RequestAsync(Under ,Name, value)
+   if Name and value then 
+      assert(type(Under) == "string", "Arg #1 should be a string insetad got "..type(Under))
+      assert(type(Name) == "string", "Arg #2 should be a string insetad got "..type(Name))
+         if IsDir(Under) == true then
+            local File = io.open(lfs.currentdir().."/"..Under.."/"..Name, 'r')
+               if File then 
+                     local RawStringContent = File:read('*all')
+                     local CookedTable = JSON.decode(RawStringContent)
+                        if type(CookedTable) == "table" then 
+                           if value then 
+                              table.insert(CookedTable, #CookedTable + 1, value)
+                              File:close()
+                              local CookedString = JSON.encode(CookedTable)
+                              local File2 = io.open(lfs.currentdir().."/"..Under.."/"..Name, 'w')
+                              File2:write(CookedString)
+                              File2:close()
+                              SniffleSay("Request Succeded")
+                           end
+                        else
+                           SniffleSay("REQUEST FAILED : Sniffle has not found a table inside of the file no changes has been made")
+                        end
+                  end
+               end
+            end
+         end 
 return Sniffle
 
--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # sniffle 
-
-- Version 0.0.1
-- Status = Unfinished
-
 A beginner friendly way to start Databases with lua
 
 
+- Version 0.0.2
+- Status = Unfinished
+
+## Update Log
+- ***Added Request Async which can be used to make a request to the module to insert data to a specifc database***
+
+- Added SniffleHelper, which allows for annotations on everything sniffle does for clarity.
+- Added a TreeManager that will be in the workspace by default so that we may update our client's tree table in the module itself 
+
+- Fixed MAJOR data loss bug + Cleaned up sniffle helper
+
+# Main
 - Basic Functions
     - [Setup](##Setup)
     - 
@@ -60,6 +68,8 @@ SniffleBase.CreateDataTree()
 
 not only initializes your directory, but it also serves as a backup in case your directory goes missing or the folder is unintentionally erased. If this occurs, it will recreate the folder (but will not completely restore all deleted or missing data) so that our code does not break.
 
+You will also see a file names tree manager being created 
+
 Then we'll use the ```SniffleBase:CreateDataBase()``` method to construct a database. CreateDatabase is a strict function, so read on carefully.
 
 Because finding dir paths in lua is difficult, and we recognize that your tree may not be immediately in the workspace, CreateBase accepts three arguments.
@@ -81,5 +91,42 @@ local PathToTree = "/workspace/foo/foo2/"
 SniffleBase:CreateDataBase("LoginInfo", "Tree",PathToTree)
 ```
 
+## Data Implementation Methods
+
+Sniffle allows you to incorporate data in a variety of ways. The first method is to manually insert it into the file, and the second is to use the sniffle module.
 
 
+Manually you have to be careful when implementing. Since its a string and sniffle handles tables we use '[]' insetad of '{}' 
+
+A good example of manual implemenetation would be something like this 
+
+Say we are structuring our login information. Since Sniffle uses tables then we will use a table with tables inside of it to store user data.
+
+Say our format is this 
+
+```lua
+{
+    {"Foo","FooPassword",10000001},
+    {"Foo2","FooPassword2",10000002}
+}
+```
+
+if we were to manual implemenet this into a txt file we would write it as this.
+```
+[["Foo","FooPassword",10000001],["Foo2","FooPassword2",10000002]]
+```
+
+This is very sensitive so it is better than we use the sniffle module so we can get 100% (or 99.9% lol) accuracy all the time
+
+```SniffleBase:RequestAsync```
+
+This makes a request to the module to implement data that can be blocked or failed due to traffic (Most likely will not happen) or errors.
+
+Arguments
+- Under - What is it data tree is under (dir name)
+ 
+ - Name - DatabaseName 
+
+ - Value - what we are inserting in 
+
+ 
